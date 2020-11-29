ea4a2b4d9f3c7be3b5644add9f1161fe24c81859
Raymond Eid <reid7@uchicago.edu>
2020-11-28 19:20:03 -0500

    cleaning repo

--- lec_3_files/ingestFlights.sh
+++
@@ -1,5 +0,0 @@
-# Unzips BTS flight data into HDFS
-# Note. I already ran on cluster. Please don't run again
-for name in *.zip
-  do unzip -p $name | tail -n +2 | hdfs dfs -put - /inputs/airline/${name%.zip}.csv;
-done
--- lec_3_files/untarWeather.sh
+++
@@ -1,7 +0,0 @@
-# Untars NOAA weather data tarballs
-# Note. I already ran on cluster. Please don't run again
-for f in *.tar;
-do
-  tar xf $f
-  rm $f
-done
